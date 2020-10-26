17e426e596492b8fea5b1e0c0cefbb2a6fce8a8d
DavidAssaraf106 <david.assaraf@polytechnique.edu>
2020-10-27 00:34:32 +0100

    Post Quizz2

--- Labs/lab4/lab4.py
+++ Labs/lab4/lab4.py
@@ -10,65 +10,69 @@ pprint = pp.pprint
 
 #### Part 1: Multiple Choice #########################################
 
-ANSWER_1 = ''
+ANSWER_1 = '2'
 
-ANSWER_2 = ''
+ANSWER_2 = '4'
 
-ANSWER_3 = ''
+ANSWER_3 = '2'
 
-ANSWER_4 = ''
+ANSWER_4 = '0'
 
-ANSWER_5 = ''
+ANSWER_5 = '3'
 
-ANSWER_6 = ''
+ANSWER_6 = '1'
 
-ANSWER_7 = ''
+ANSWER_7 = '0'
 
 #### Part 2: Transitive Rule #########################################
 
 # Fill this in with your rule 
-transitive_rule = IF( AND( ), THEN( ) )
+transitive_rule = IF(AND('(?x) beats (?y)',
+                         '(?y) beats (?z)'), THEN('(?x) beats (?z)'))
 
 # You can test your rule by uncommenting these pretty print statements
 #  and observing the results printed to your screen after executing lab1.py
-# pprint(forward_chain([transitive_rule], abc_data))
-# pprint(forward_chain([transitive_rule], poker_data))
-# pprint(forward_chain([transitive_rule], minecraft_data))
+#pprint(forward_chain([transitive_rule], abc_data))
+#pprint(forward_chain([transitive_rule], poker_data))
+#pprint(forward_chain([transitive_rule], minecraft_data))
 
 
 #### Part 3: Family Relations #########################################
-
+# create a rule: state two persons are different
 # Define your rules here. We've given you an example rule whose lead you can follow:
-friend_rule = IF( AND("person (?x)", "person (?y)"), THEN ("friend (?x) (?y)", "friend (?y) (?x)") )
-
-
+friend_rule0 = IF("person (?x)", THEN("(?x) and (?x) are same"))
+friend_rule1 = IF(AND('parent (?x) (?y)', 'parent (?z) (?x)'), THEN('grandparent (?z) (?y)', 'grandchild (?y) (?z)'))
+friend_rule2 = IF(AND('parent (?x) (?y)', 'parent (?x) (?z)', NOT('(?y) and (?z) are same')), THEN('sibling (?y) (?z)', 'sibling (?z) (?y)'))
+friend_rule3 = IF('parent (?x) (?y)', THEN('child (?y) (?x)'))
+friend_rule4 = IF(AND('parent (?x) (?y)', 'parent (?z) (?w)', 'sibling (?x) (?z)', NOT('sibling (?y) (?w)')), THEN('cousin (?y) (?w)', 'cousin (?w) (?y)'))
 
 
 # Add your rules to this list:
-family_rules = [ friend_rule ]
+family_rules = [friend_rule0, friend_rule1, friend_rule2, friend_rule3, friend_rule4]
 
 # Uncomment this to test your data on the Simpsons family:
-# pprint(forward_chain(family_rules, simpsons_data, verbose=False))
+pprint(forward_chain(family_rules, simpsons_data, verbose=True))
 
 # These smaller datasets might be helpful for debugging:
-# pprint(forward_chain(family_rules, sibling_test_data, verbose=True))
-# pprint(forward_chain(family_rules, grandparent_test_data, verbose=True))
+pprint(forward_chain(family_rules, sibling_test_data, verbose=True))
+pprint(forward_chain(family_rules, grandparent_test_data, verbose=True))
 
 # The following should generate 14 cousin relationships, representing 7 pairs
 # of people who are cousins:
 harry_potter_family_cousins = [
     relation for relation in
     forward_chain(family_rules, harry_potter_family_data, verbose=False)
-    if "cousin" in relation ]
+    if "cousin" in relation]
 
 # To see if you found them all, uncomment this line:
-# pprint(harry_potter_family_cousins)
+pprint(harry_potter_family_cousins)
 
 
 #### Part 4: Backward Chaining #########################################
 
 # Import additional methods for backchaining
 from production import PASS, FAIL, match, populate, simplify, variables
+
 
 def backchain_to_goal_tree(rules, hypothesis):
     """
@@ -83,7 +87,24 @@ def backchain_to_goal_tree(rules, hypothesis):
     (possibly with unbound variables), *not* AND or OR objects.
     Make sure to use simplify(...) to flatten trees where appropriate.
     """
-    raise NotImplementedError
+    matches = [hypothesis]  # always OR
+    for rule in rules:
+        var = match(rule.consequent(), hypothesis)
+        if var != None:
+            antecedent = rule.antecedent()
+            antecedent_matches = []
+            next_hypo = populate(antecedent, var)  # create a rule
+
+            if type(antecedent) == str:  # backtrack
+                antecedent_matches.append(backchain_to_goal_tree(rules, next_hypo))
+            else:
+                for a in next_hypo:
+                    antecedent_matches.append(backchain_to_goal_tree(rules, a))
+            if type(antecedent) == OR:
+                matches.append(OR(antecedent_matches))
+            else:
+                matches.append(AND(antecedent_matches))
+    return simplify(OR(matches))
 
 
 # Uncomment this to test out your backward chainer:
@@ -98,7 +119,6 @@ HOW_MANY_HOURS_THIS_LAB_TOOK = None
 WHAT_I_FOUND_INTERESTING = None
 WHAT_I_FOUND_BORING = None
 SUGGESTIONS = None
-
 
 ###########################################################
 ### Ignore everything below this line; for testing only ###
--- Labs/lab5/lab5.py
+++ Labs/lab5/lab5.py
@@ -2,21 +2,50 @@
 # Written by 6.034 staff
 
 from nets import *
-
+from bayes_api import *
+import numpy as np
 
 #### Part 1: Warm-up; Ancestors, Descendents, and Non-descendents ##############
 
 def get_ancestors(net, var):
     "Return a set containing the ancestors of var"
-    raise NotImplementedError
+    parents_visited = []
+    resulting_set = set()
+    direct_parents = net.get_parents(var)
+    parents_list = [parent for parent in direct_parents]
+    while parents_list:
+        parent = parents_list.pop()
+        resulting_set.add(parent)
+        if parent not in parents_visited:
+            parents_visited.append(parent)
+            parents_list.extend(net.get_parents(parent))
+    return resulting_set
+
+
 
 def get_descendants(net, var):
     "Returns a set containing the descendants of var"
-    raise NotImplementedError
+    children_visited = []
+    resulting_set = set()
+    direct_children = net.get_children(var)
+    children_list = [child for child in direct_children]
+    while children_list:
+        child = children_list.pop()
+        resulting_set.add(child)
+        if child not in children_visited:
+            children_visited.append(child)
+            children_list.extend(net.get_children(child))
+    return resulting_set
 
 def get_nondescendants(net, var):
     "Returns a set containing the non-descendants of var"
-    raise NotImplementedError
+    variables = net.get_variables()
+    descendants_var = get_descendants(net, var)
+    for descendant in descendants_var:
+        variables.remove(descendant)
+    variables.remove(var)
+    return set(variables)
+
 
 
 #### Part 2: Computing Probability #############################################
@@ -28,27 +57,69 @@ def simplify_givens(net, var, givens):
     givens.  Otherwise, if not all parents are given, or if a descendant is
     given, returns original givens.
     """
-    raise NotImplementedError
+    parent = net.get_parents(var)
+    descendants = get_descendants(net, var)
+    if parent.issubset(givens) and not descendants.intersection(givens):
+        return {p: givens.get(p) for p in parent}
+    return givens
+
     
 def probability_lookup(net, hypothesis, givens=None):
     "Looks up a probability in the Bayes net, or raises LookupError"
-    raise NotImplementedError
+    if givens is None:
+        try:
+            return net.get_probability(hypothesis, None, infer_missing=True)
+        except ValueError:
+            raise LookupError
+    try:
+        simplified_givens = simplify_givens(net, list(hypothesis)[0], givens)
+        probability = net.get_probability(hypothesis, simplified_givens, infer_missing=True)
+        return probability
+    except ValueError:
+        raise LookupError
 
 def probability_joint(net, hypothesis):
     "Uses the chain rule to compute a joint probability"
-    raise NotImplementedError
+    ordered_net = net.topological_sort()[::-1]  # children come before their parents
+    p = 1
+    for v in ordered_net:
+        if v in hypothesis:
+            val = hypothesis.pop(v)
+            if hypothesis is not None:
+                prob = probability_lookup(net, {v:val}, givens=hypothesis)
+            else:
+                prob = probability_lookup(net, {v:val}, givens=None)
+            p = p*prob
+    return p
+
+
+
     
 def probability_marginal(net, hypothesis):
     "Computes a marginal probability as a sum of joint probabilities"
-    raise NotImplementedError
+    joints = net.combinations(net.get_variables(), hypothesis)
+    joint_probabilities = [probability_joint(net, joint) for joint in joints]
+    return np.sum(joint_probabilities)
+
 
 def probability_conditional(net, hypothesis, givens=None):
     "Computes a conditional probability as a ratio of marginal probabilities"
-    raise NotImplementedError
+    if givens is not None:
+        for k in hypothesis.keys():
+            if k in givens.keys():
+                if hypothesis.get(k) != givens.get(k):
+                    return 0
+        joint_hypothesis = dict(hypothesis, **givens)
+        numerator = probability_marginal(net, joint_hypothesis)
+        denominator = probability_marginal(net, givens)
+        return numerator/denominator
+    else:
+        return probability_marginal(net, hypothesis)
     
 def probability(net, hypothesis, givens=None):
     "Calls previous functions to compute any probability"
-    raise NotImplementedError
+    return probability_conditional(net, hypothesis, givens)
+
 
 
 #### Part 3: Counting Parameters ###############################################
@@ -57,7 +128,19 @@ def number_of_parameters(net):
     """
     Computes the minimum number of parameters required for the Bayes net.
     """
-    raise NotImplementedError
+    params = 0
+    all_variables = net.get_variables()
+    for variable in all_variables:
+        domain_variable = len(net.get_domain(variable)) - 1
+        parents = net.get_parents(variable)
+        if len(parents) > 0:
+            p = 1
+            for parent in parents:
+                p *= len(net.get_domain(parent))
+            params += p*domain_variable
+        else:
+            params += domain_variable
+    return params
 
 
 #### Part 4: Independence ######################################################
@@ -67,7 +150,18 @@ def is_independent(net, var1, var2, givens=None):
     Return True if var1, var2 are conditionally independent given givens,
     otherwise False. Uses numerical independence.
     """
-    raise NotImplementedError
+    hypotheses = net.combinations([var1, var2])
+    for hypothese in hypotheses:
+        if givens is None:
+            marginal_probability = probability(net, {var1: hypothese.get(var1)}, None)
+            conditional_probability = probability(net, {var1: hypothese.get(var1)}, {var2: hypothese.get(var2)})
+        else:
+            marginal_probability = probability(net, {var1: hypothese.get(var1)}, givens)
+            conditional_probability = probability(net, {var1: hypothese.get(var1)}, dict(givens, **{var2: hypothese.get(var2)}))
+        if not (approx_equal(marginal_probability, conditional_probability)):
+            return False
+    return True
+
     
 def is_structurally_independent(net, var1, var2, givens=None):
     """
@@ -75,14 +169,46 @@ def is_structurally_independent(net, var1, var2, givens=None):
     based on the structure of the Bayes net, otherwise False.
     Uses structural independence only (not numerical independence).
     """
-    raise NotImplementedError
+    # construct the ancestor graph
+    ancestor_variables = set()
+    # how to deal with givens = None ?
+    if givens is not None:
+        for var in [var1, var2, *givens.keys()]:
+            ancestor_variables.update(get_ancestors(net, var))
+        ancestor_variables.update({var1, var2, *givens.keys()})
+    if givens is None:
+        for var in [var1, var2]:
+            ancestor_variables.update(get_ancestors(net, var))
+        ancestor_variables.update({var1, var2})
+    ancestor_net = net.subnet(list(ancestor_variables))
+    # link parents of a same child in the ancestor graph
+    variables = ancestor_net.get_variables()
+    for var in variables:
+        parents = ancestor_net.get_parents(var)
+        for i, parent1 in enumerate(list(parents)):
+            for parent2 in list(parents)[i+1:]:
+                if parent1 != parent2:
+                    ancestor_net.link(parent1, parent2)
+    # disorient the graph
+    ancestor_net = ancestor_net.make_bidirectional()
+    # delete the givens and their edges
+    if givens is not None:
+        for given in givens:
+            if given in ancestor_net.get_variables():
+                ancestor_net.remove_variable(given)
+    # check for connection inside the remaining graph
+    return ancestor_net.find_path(var1, var2) is None
+
+
+
+
 
 
 #### SURVEY ####################################################################
 
-NAME = None
-COLLABORATORS = None
-HOW_MANY_HOURS_THIS_LAB_TOOK = None
-WHAT_I_FOUND_INTERESTING = None
-WHAT_I_FOUND_BORING = None
-SUGGESTIONS = None
+NAME = 'Assaraf David'
+COLLABORATORS = 'None'
+HOW_MANY_HOURS_THIS_LAB_TOOK = 7
+WHAT_I_FOUND_INTERESTING = 'Everything'
+WHAT_I_FOUND_BORING = 'None'
+SUGGESTIONS = 'None'
